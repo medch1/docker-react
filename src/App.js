2a3f4876f8edb0260a6baf295ff3aa841eac8768
medch1 <chachia.mohamed@gmail.com>
2022-04-05 23:01:17 +0100

    change app text with branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Changed !
+          Changed in feature branch !
         </p>
         <a
           className="App-link"
